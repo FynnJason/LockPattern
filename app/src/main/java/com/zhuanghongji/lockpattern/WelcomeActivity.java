38061288a372f0ea6d0324b30b13bc84bcdc6ffb
FynnJason <410132905@qq.com>
2017-10-16 17:31:34 +0800

    Initial commit

---
+++ app/src/main/java/com/zhuanghongji/lockpattern/WelcomeActivity.java
@@ -0,0 +1,33 @@
+package com.zhuanghongji.lockpattern;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zhuanghongji.lockpattern.util.PreferenceUtil;
+
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                String passwordStr = PreferenceUtil.getGesturePassword(WelcomeActivity.this);
+                Intent intent;
+                if (passwordStr.equals("")) {
+                    intent = new Intent(WelcomeActivity.this, SetLockActivity.class);
+                } else {
+                    intent = new Intent(WelcomeActivity.this, UnlockActivity.class);
+                }
+                startActivity(intent);
+                WelcomeActivity.this.finish();
+            }
+        }, 2000);
+    }
+}
